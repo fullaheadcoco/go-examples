b2955d13060caced3035168634978cf17842d033
eco <kjlee.main@gmail.com>
2022-03-28 09:59:57 +0900

    add basic examples

---
+++ basic/6_reverseString/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "hello world"
+	fmt.Println(str1)
+
+	str2 := Reverse(str1)
+	fmt.Println(str2)
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}

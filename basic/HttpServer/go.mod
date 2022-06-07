c1c6ce1c52ba42f4606c883a8f04d4e341d24934
fullaheadcoco <kjlee.main@gmail.com>
2022-06-07 09:43:25 +0900

    basic/HttpServer 예제 추가

---
+++ basic/HttpServer/go.mod
@@ -0,0 +1,3 @@
+module github.com/fullagheadcoco/go-examples/basic/HttpServer
+
+go 1.18
---
+++ basic/HttpServer/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+/**
+$ go run main.go &
+$ curl localhost:8090/ctx
+server: hello handler started
+^C
+server: context canceled
+server: hello handler ended
+*/
+func ctx(w http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/ctx", ctx)
+
+	http.ListenAndServe(":8090", nil)
+}
